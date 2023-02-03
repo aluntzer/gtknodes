c5ec0d9bf796b7e89eb2cb8f54dc216d3df2f4e3
Armin Luntzer <armin.luntzer@univie.ac.at>
2023-02-03 08:00:10 +0100

    g_once_init_leave() interface may have changed at some point; remove volatile qualifier

--- src/gtknodesocket.c
+++ src/gtknodesocket.c
@@ -1574,9 +1574,9 @@ gtk_nodes_node_socket_new (void)
 GType
 gtk_nodes_node_socket_io_get_type (void)
 {
-  static volatile gsize g_define_type_id__volatile = 0;
+  static gsize g_define_type_id__ = 0;
 
-  if (g_once_init_enter (&g_define_type_id__volatile))
+  if (g_once_init_enter (&g_define_type_id__))
     {
       static const GEnumValue values[] = {
         { GTKNODES_NODE_SOCKET_DISABLE, "GTKNODES_NODE_SOCKET_DISABLE", "disabled" },
@@ -1586,8 +1586,8 @@ gtk_nodes_node_socket_io_get_type (void)
       };
       GType g_define_type_id =
         g_enum_register_static (g_intern_static_string ("GtkNodesNodeSocketType"), values);
-      g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);
+      g_once_init_leave (&g_define_type_id__, g_define_type_id);
     }
 
-  return g_define_type_id__volatile;
+  return g_define_type_id__;
 }
